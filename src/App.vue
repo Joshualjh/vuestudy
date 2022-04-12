4127631e266dd53b75c4c46e86c8a205d3cf97ea
unknown <aak1009@naver.com>
2022-04-12 23:34:45 +0900

    3

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>원룸샵</h1>
+    <h4>{{products[0]}} 원룸</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{products[1]}} 원룸</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
+
+  <div>
+    <h4>{{products[2]}} 원룸</h4>
+    <p>{{ price3 }} 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data() {
+    return {
+      price1 : 60, 
+      price2 : 70,
+      price3 : 80,
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
